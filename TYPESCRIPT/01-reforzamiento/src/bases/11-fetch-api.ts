07046d7e156098d1b836c6f16c2e41f355e2b18d
Portalo <smiguezc01@iesarroyoharnina.es>
2025-12-21 18:39:50 +0100

    fetch and interrfaces

--- TYPESCRIPT/01-reforzamiento/src/bases/11-fetch-api.ts
+++ TYPESCRIPT/01-reforzamiento/src/bases/11-fetch-api.ts
@@ -1,24 +1,39 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
 
 
-const API_KEY = 'ew7x9CzKaoxcqQM72cgs737vyEVIHYty';
+const API_KEY = import.meta.env.VITE_API_KEY;
 // const URI = 'http://api.giphy.com/v1/gifs/trending';
 const URI = 'http://api.giphy.com/v1/gifs/random';
 
 
 const myRequest = fetch(`${URI}?api_key=${API_KEY}`);
-                   
 
-myRequest.then( (response) =>{
+const createImageInsideDOM = ( url: string ): void =>{
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
 
-    response.json().then( (data) => {
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl);
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
+    document.body.append(imgElement);
+}
 
-        document.body.append(imgElement);
-    });
-})
-.catch((err)=>{
-    console.log(err);
-})
+myRequest.then((response) => response.json())
+        .then( ({data}: GiphyRandomResponse) => {
+            const imageUrl: string = data.images.original.url;
+            createImageInsideDOM(imageUrl);
+        })
+        .catch( (err) => console.log(err));
+
+// Versión larga
+// myRequest.then( (response) =>{
+
+//     response.json().then( (data) => {
+//         const imageUrl = data.data.images.original.url;
+//         console.log(imageUrl);
+//         const imgElement = document.createElement('img');
+//         imgElement.src = imageUrl;
+
+//         document.body.append(imgElement);
+//     });
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
